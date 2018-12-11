af9719e968169fa12773f9cd43699d97c32bcdfe
Radames Cruz Moreno <v-rcruz@microsoft.com>
2018-12-11 15:50:19 -0800

    Add CMakeLists.txt to compile SEALdll in Linux

--- net/dll/seal/defines.h
+++ net/dll/seal/defines.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#ifdef _MSC_VER
+
 #ifdef SEALDLL_EXPORTS
 #define SEALDLL extern "C" __declspec(dllexport)
 #else
@@ -10,3 +12,24 @@
 #endif
 
 #define SEALCALL __cdecl
+
+#else // _MSC_VER
+
+#define SEALDLL extern "C"
+#define SEALCALL
+
+#define HRESULT long
+
+#define _HRESULT_TYPEDEF_(hr)   ((HRESULT)hr)
+
+#define E_POINTER               _HRESULT_TYPEDEF_(0x80004003L)
+#define E_INVALIDARG            _HRESULT_TYPEDEF_(0x80070057L)
+#define E_OUTOFMEMORY           _HRESULT_TYPEDEF_(0x8007000EL)
+#define E_UNEXPECTED            _HRESULT_TYPEDEF_(0x8000FFFFL)
+//#define E_POINTER               _HRESULT_TYPEDEF_(0x80004003L)
+//#define E_POINTER               _HRESULT_TYPEDEF_(0x80004003L)
+
+#define S_OK                    _HRESULT_TYPEDEF_(0L)
+
+#endif // _MSC_VER
+
