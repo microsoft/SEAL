d3cc1099df6aa13aaef42e195166db1e62fb42ba
Radames S. Cruz Moreno <v-rcruz@microsoft.com>
2019-01-30 14:03:41 -0800

    Fix XML Doc warning in IntegerEncoder

--- dotnet/src/IntegerEncoder.cs
+++ dotnet/src/IntegerEncoder.cs
@@ -35,9 +35,8 @@ namespace Microsoft.Research.SEAL
         /// </summary>
         /// <param name="context">The SEALContext</param>
         /// <exception cref="ArgumentNullException">if context is null</exception>
-        /// <exception cref="ArgumentException"> if the context is not set 
-        /// <exception cref="ArgumentException">if the PlainModulus set in context is not 
-        /// at least 2</exception>
+        /// <exception cref="ArgumentException">if the context is not set 
+        /// <exception cref="ArgumentException">if the PlainModulus set in context is not at least 2</exception>
         public IntegerEncoder(SEALContext context)
         {
             if (null == context)
