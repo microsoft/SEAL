ddf695c65bb866809caac6d7516dad23713ba227
Wei Dai <wei.dai@microsoft.com>
2019-02-15 11:53:56 -0800

    added CKKS to native TestEncryptionParameters

--- native/tests/seal/encryptionparams.cpp
+++ native/tests/seal/encryptionparams.cpp
@@ -12,39 +12,51 @@ namespace SEALTest
 {
     TEST(EncryptionParametersTest, EncryptionParametersSet)
     {
-        auto scheme = scheme_type::BFV;
-        EncryptionParameters parms(scheme);
-        parms.set_noise_standard_deviation(3.20);
-        parms.set_coeff_modulus({ 2, 3 });
-        parms.set_plain_modulus(2);
-        parms.set_poly_modulus_degree(2);
-        parms.set_random_generator(UniformRandomGeneratorFactory::default_factory());
+        auto encryption_parameters_test = [](scheme_type scheme)
+        {
+            EncryptionParameters parms(scheme);
+            parms.set_noise_standard_deviation(3.20);
+            parms.set_coeff_modulus({ 2, 3 });
+            if (scheme == scheme_type::BFV)
+                parms.set_plain_modulus(2);
+            parms.set_poly_modulus_degree(2);
+            parms.set_random_generator(UniformRandomGeneratorFactory::default_factory());
 
-        ASSERT_TRUE(scheme == parms.scheme());
-        ASSERT_EQ(3.20, parms.noise_standard_deviation());
-        ASSERT_EQ(3.20 * 6, parms.noise_max_deviation());
-        ASSERT_TRUE(parms.coeff_modulus()[0] == 2);
-        ASSERT_TRUE(parms.coeff_modulus()[1] == 3);
-        ASSERT_TRUE(parms.plain_modulus() == 2);
-        ASSERT_TRUE(parms.poly_modulus_degree() == 2);
-        ASSERT_TRUE(parms.random_generator() == UniformRandomGeneratorFactory::default_factory());
+            ASSERT_TRUE(scheme == parms.scheme());
+            ASSERT_EQ(3.20, parms.noise_standard_deviation());
+            ASSERT_EQ(3.20 * 6, parms.noise_max_deviation());
+            ASSERT_TRUE(parms.coeff_modulus()[0] == 2);
+            ASSERT_TRUE(parms.coeff_modulus()[1] == 3);
+            if (scheme == scheme_type::BFV)
+                ASSERT_TRUE(parms.plain_modulus() == 2);
+            else if (scheme == scheme_type::CKKS)
+                ASSERT_TRUE(parms.plain_modulus() == NULL);
+            ASSERT_TRUE(parms.poly_modulus_degree() == 2);
+            ASSERT_TRUE(parms.random_generator() == UniformRandomGeneratorFactory::default_factory());
 
-        parms.set_noise_standard_deviation(3.20);
-        parms.set_coeff_modulus({
-            DefaultParams::small_mods_30bit(0), DefaultParams::small_mods_40bit(0), DefaultParams::small_mods_50bit(0)
-            });
-        parms.set_plain_modulus(2);
-        parms.set_poly_modulus_degree(128);
-        parms.set_random_generator(UniformRandomGeneratorFactory::default_factory());
+            parms.set_noise_standard_deviation(3.20);
+            parms.set_coeff_modulus({
+                DefaultParams::small_mods_30bit(0), DefaultParams::small_mods_40bit(0), DefaultParams::small_mods_50bit(0)
+                });
+            if (scheme == scheme_type::BFV)
+                parms.set_plain_modulus(2);
+            parms.set_poly_modulus_degree(128);
+            parms.set_random_generator(UniformRandomGeneratorFactory::default_factory());
 
-        ASSERT_EQ(3.20, parms.noise_standard_deviation());
-        ASSERT_EQ(3.20 * 6, parms.noise_max_deviation());
-        ASSERT_TRUE(parms.coeff_modulus()[0] == DefaultParams::small_mods_30bit(0));
-        ASSERT_TRUE(parms.coeff_modulus()[1] == DefaultParams::small_mods_40bit(0));
-        ASSERT_TRUE(parms.coeff_modulus()[2] == DefaultParams::small_mods_50bit(0));
-        ASSERT_TRUE(parms.plain_modulus() == 2);
-        ASSERT_TRUE(parms.poly_modulus_degree() == 128);
-        ASSERT_TRUE(parms.random_generator() == UniformRandomGeneratorFactory::default_factory());
+            ASSERT_EQ(3.20, parms.noise_standard_deviation());
+            ASSERT_EQ(3.20 * 6, parms.noise_max_deviation());
+            ASSERT_TRUE(parms.coeff_modulus()[0] == DefaultParams::small_mods_30bit(0));
+            ASSERT_TRUE(parms.coeff_modulus()[1] == DefaultParams::small_mods_40bit(0));
+            ASSERT_TRUE(parms.coeff_modulus()[2] == DefaultParams::small_mods_50bit(0));
+            if (scheme == scheme_type::BFV)
+                ASSERT_TRUE(parms.plain_modulus() == 2);
+            else if (scheme == scheme_type::CKKS)
+                ASSERT_TRUE(parms.plain_modulus() == NULL);
+            ASSERT_TRUE(parms.poly_modulus_degree() == 128);
+            ASSERT_TRUE(parms.random_generator() == UniformRandomGeneratorFactory::default_factory());
+        };
+        encryption_parameters_test(scheme_type::BFV);
+        encryption_parameters_test(scheme_type::CKKS);
     }
 
     TEST(EncryptionParametersTest, EncryptionParametersCompare)
@@ -53,7 +65,8 @@ namespace SEALTest
         EncryptionParameters parms1(scheme);
         parms1.set_noise_standard_deviation(3.20);
         parms1.set_coeff_modulus({ DefaultParams::small_mods_30bit(0) });
-        parms1.set_plain_modulus(1 << 6);
+        if (scheme == scheme_type::BFV)
+            parms1.set_plain_modulus(1 << 6);
         parms1.set_poly_modulus_degree(64);
         parms1.set_random_generator(UniformRandomGeneratorFactory::default_factory());
 
@@ -78,7 +91,8 @@ namespace SEALTest
         ASSERT_FALSE(parms3 == parms1);
 
         parms3 = parms2;
-        parms3.set_plain_modulus((1 << 6) + 1);
+        if (scheme == scheme_type::BFV)
+            parms3.set_plain_modulus((1 << 6) + 1);
         ASSERT_FALSE(parms3 == parms2);
 
         parms3 = parms2;
@@ -110,7 +124,8 @@ namespace SEALTest
         EncryptionParameters parms2(scheme);
         parms.set_noise_standard_deviation(3.20);
         parms.set_coeff_modulus({ DefaultParams::small_mods_30bit(0) });
-        parms.set_plain_modulus(1 << 6);
+        if (scheme == scheme_type::BFV)
+            parms.set_plain_modulus(1 << 6);
         parms.set_poly_modulus_degree(64);
         EncryptionParameters::Save(parms, stream);
         parms2 = EncryptionParameters::Load(stream);
@@ -126,7 +141,8 @@ namespace SEALTest
         parms.set_coeff_modulus({
             DefaultParams::small_mods_30bit(0), DefaultParams::small_mods_60bit(0), DefaultParams::small_mods_60bit(1)
             });
-        parms.set_plain_modulus(1 << 30);
+        if (scheme == scheme_type::BFV)
+            parms.set_plain_modulus(1 << 30);
         parms.set_poly_modulus_degree(256);
 
         EncryptionParameters::Save(parms, stream);
