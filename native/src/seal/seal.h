f8c9b3f6af83e168d3a87548238c866f6003171f
Kim Laine <kim.laine@microsoft.com>
2019-06-20 19:58:24 -0700

    Added serialization.h to seal.h

--- native/src/seal/seal.h
+++ native/src/seal/seal.h
@@ -23,5 +23,6 @@
 #include "seal/randomtostd.h"
 #include "seal/relinkeys.h"
 #include "seal/secretkey.h"
+#include "seal/serialization.h"
 #include "seal/smallmodulus.h"
 #include "seal/valcheck.h"
