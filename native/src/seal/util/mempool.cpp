0f47366ce0b1bb4bd196c3b0b3059111fbe00cc8
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2020-08-27 11:40:59 -0700

    Small changes to zero memory functions

--- native/src/seal/util/mempool.cpp
+++ native/src/seal/util/mempool.cpp
@@ -82,7 +82,7 @@ namespace seal
                 for (auto &alloc : allocs_)
                 {
                     size_t curr_alloc_byte_count = mul_safe(item_byte_count_, alloc.size);
-                    seal::util::memzero(alloc.data_ptr, curr_alloc_byte_count);
+                    seal_memzero(alloc.data_ptr, curr_alloc_byte_count);
 
                     // Delete this allocation
                     delete[] alloc.data_ptr;
