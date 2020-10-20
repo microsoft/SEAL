449b6f7612d00edc1b950140249416dd00abe6e2
Yo Mero <yomero@gmail.com>
2020-10-20 16:10:29 -0700

    Add memset cmake file

---
+++ cmake/memset_s.iOS.cmake
@@ -0,0 +1,11 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+set( SEAL_MEMSET_S_FOUND_EXITCODE 
+     "0"
+     CACHE STRING "Result from TRY_RUN" FORCE)
+
+set( SEAL_MEMSET_S_FOUND_EXITCODE__TRYRUN_OUTPUT 
+     ""
+     CACHE STRING "Output from TRY_RUN" FORCE)
+
