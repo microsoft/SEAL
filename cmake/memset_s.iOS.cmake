a4b46f307a51fb5e51b2d0da2eebcd42233bf8d7
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2020-11-05 09:59:09 -0800

    Add memset_s comment

--- cmake/memset_s.iOS.cmake
+++ cmake/memset_s.iOS.cmake
@@ -1,6 +1,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
 
+# This cache-initialization file will be used to enable memset_s when cross-compiling
+# for iOS, as check_cxx_source_runs fails for cross-compilers.
+
 set( SEAL_MEMSET_S_FOUND_EXITCODE 
      "0"
      CACHE STRING "Result from TRY_RUN" FORCE)
