4aac87b10089810181d94ff56fb6d911a37f1a5e
Bastien Vialla <bastien.vialla@orange.com>
2021-01-18 14:10:24 +0100

    Add support ARM64 intrinsics
    Add architecture check in the files CheckCXXIntrinsicsHeader.cmake, gcc.h andclang.h to include the correct intrinsics header when compiling on arm64.

--- cmake/CheckCXXIntrinsicsHeader.cmake
+++ cmake/CheckCXXIntrinsicsHeader.cmake
@@ -6,7 +6,21 @@ if(SEAL_USE_INTRIN)
     if(MSVC)
         set(SEAL_INTRIN_HEADER "intrin.h")
     else()
-        set(SEAL_INTRIN_HEADER "x86intrin.h")
+        #set(CMAKE_REQUIRED_QUIET TRUE)
+        check_cxx_source_runs("
+            #if defined(__x86_64__)
+                #error
+            #endif
+            int main() {
+                return 0;
+            }"
+            SEAL_ARM64
+        )
+        if(SEAL_ARM64)
+            set(SEAL_INTRIN_HEADER "arm_neon.h")    
+        else()
+            set(SEAL_INTRIN_HEADER "x64intrin.h")
+        endif()
     endif()
 
     check_include_file_cxx(${SEAL_INTRIN_HEADER} SEAL_INTRIN_HEADER_FOUND)
