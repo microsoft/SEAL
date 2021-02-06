ad22efc374988fe6fd2c9cb0e0bc74657da3c299
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2021-02-05 17:45:05 -0800

    Rename memset_s.iOS.cmake
    Rename to functions.iOS.cmake, since now it contains more than memset_s

--- cmake/functions.iOS.cmake
+++ cmake/functions.iOS.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
 
-# This cache-initialization file will be used to enable memset_s when cross-compiling
+# This cache-initialization file will be used to enable functions when cross-compiling
 # for iOS, as check_cxx_source_runs fails for cross-compilers.
 
 set(SEAL_MEMSET_S_FOUND_EXITCODE
