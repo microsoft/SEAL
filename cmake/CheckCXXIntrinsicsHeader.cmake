a8a320e9cc216130da1acdd957472ee44dcd4a65
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2021-02-08 17:02:09 -0800

    Detect ARM processor

--- cmake/CheckCXXIntrinsicsHeader.cmake
+++ cmake/CheckCXXIntrinsicsHeader.cmake
@@ -6,14 +6,15 @@ if(SEAL_USE_INTRIN)
     if(MSVC)
         set(SEAL_INTRIN_HEADER "intrin.h")
     else()
-        #set(CMAKE_REQUIRED_QUIET TRUE)
         check_cxx_source_runs("
-            #if defined(__x86_64__)
+            #if defined(__arm__) || defined(__aarch64__)
+                int main() {
+                    return 0;
+                }
+            #else
                 #error
             #endif
-            int main() {
-                return 0;
-            }"
+            "
             SEAL_ARM64
         )
         if(SEAL_ARM64)
