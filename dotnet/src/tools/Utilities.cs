781c16297ccb9878c439619776127c1810d79461
Kim Laine <kim.laine@microsoft.com>
2021-04-06 18:57:19 -0700

    Added missing using statement

--- dotnet/src/tools/Utilities.cs
+++ dotnet/src/tools/Utilities.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+using System;
+
 namespace Microsoft.Research.SEAL.Tools
 {
     static class Utilities
