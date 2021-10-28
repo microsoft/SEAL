df996195c175d5daaa43a8ce01457f5e96ecfe75
Akshaya Jagannadharao <akshaya.jagannadharao@intel.com>
2021-10-28 10:55:58 -0700

    adding CI

---
+++ .github/config-tests/config_5.sh
@@ -0,0 +1,29 @@
+# Build with pre-built HEXL
+set -x
+COMPILER_FLAGS="-DCMAKE_BUILD_TYPE=Release
+                -DCMAKE_CXX_COMPILER=clang++
+                -DCMAKE_C_COMPILER=clang
+                -DSEAL_BUILD_TESTS=OFF
+                -DSEAL_BUILD_BENCH=OFF
+                -DSEAL_BUILD_EXAMPLES=ON
+                -DSEAL_USE_INTEL_HEXL=ON
+                -DSEAL_BUILD_DEPS=OFF
+                -DSEAL_BUILD_SEAL_C=OFF
+                -DSEAL_USE_MSGSL=OFF
+                -DSEAL_USE_ZLIB=OFF
+                -DSEAL_USE_ZSTD=OFF
+                -DBUILD_SHARED_LIBS=OFF
+                -DSEAL_USE_CXX17=OFF
+                -DCMAKE_INSTALL_PREFIX=./"
+
+cd hexl
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./
+cmake --build build -j --config Release
+cmake --install build --config Release
+cd ..
+export HEXL_DIR=$(pwd)/hexl/lib/cmake/hexl-$HEXL_VER
+ls ${HEXL_DIR}
+cmake -B build ${COMPILER_FLAGS} -DCMAKE_MODULE_PATH=${HEXL_DIR} -DHEXL_DIR=${HEXL_DIR}
+cmake --build build -j --config Release
+cmake --build build -j --target install --config Release
+exit $?
