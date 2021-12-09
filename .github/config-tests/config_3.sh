6b330d16287246c9d0824d000b66b885965d2d38
Akshaya Jagannadharao <akshaya.jagannadharao@intel.com>
2021-12-08 16:03:00 -0800

    try bin/Debug

--- .github/config-tests/config_3.sh
+++ .github/config-tests/config_3.sh
@@ -15,11 +15,10 @@ COMPILER_FLAGS="-DCMAKE_BUILD_TYPE=Debug
 cmake -B build ${COMPILER_FLAGS}
 cmake --build build -j --config Debug
 cmake --build build -j --target install --config Debug
-sleep 3
-ls -la build
-ls -la build/bin
-find . -name "sealtest"
-build/bin/sealtest --gtest_output=xml
+# ls -la build
+# ls -la build/bin
+# find . -name "sealtest"
+build/bin/Debug/sealtest --gtest_output=xml
 
 # Build examples using pre-built SEAL
 export SEAL_DIR=$(pwd)/lib/cmake/SEAL-$SEAL_VER/
