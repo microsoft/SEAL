c560c6ae0be98c3154d6e8a7444ed8a43a112714
Akshaya Jagannadharao <akshaya.jagannadharao@intel.com>
2021-12-09 13:24:39 -0800

    Debug

--- .github/config-tests/config_3.sh
+++ .github/config-tests/config_3.sh
@@ -1,7 +1,7 @@
 # Build in debug mode, with prebuilt SEAL, and with no HEXL. Finally, run seal examples.
 set -x
 COMPILER_FLAGS="-DCMAKE_BUILD_TYPE=Debug
-                -DMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG=build/bin
+                -DMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG=build/bin/Debug
                 -DCMAKE_CXX_COMPILER=g++
                 -DCMAKE_C_COMPILER=gcc
                 -DSEAL_BUILD_TESTS=ON
@@ -16,10 +16,10 @@ COMPILER_FLAGS="-DCMAKE_BUILD_TYPE=Debug
 cmake -B build ${COMPILER_FLAGS}
 cmake --build build -j --config Debug
 cmake --build build -j --target install --config Debug
-build/bin/sealtest --gtest_output=xml
+build/bin/Debug/sealtest --gtest_output=xml
 
 ls -la build/bin
-ls -la buil/bin/Debug
+ls -la build/bin/Debug
 find . -name "sealtest"
 # Build examples using pre-built SEAL
 export SEAL_DIR=$(pwd)/lib/cmake/SEAL-$SEAL_VER/
@@ -29,5 +29,5 @@ cmake -B build -DSEAL_DIR=${SEAL_DIR} -DCMAKE_MODULE_PATH=${SEAL_DIR}
 cmake --build build -j
 
 # Run examples 1, 2, 3, 4, 5, and 6 before exiting (0)
-echo 1 2 3 4 5 6 0 | build/bin/sealexamples
+echo 1 2 3 4 5 6 0 | build/bin/Debug/sealexamples
 exit $?
--- .github/config-tests/config_4.sh
+++ .github/config-tests/config_4.sh
@@ -1,6 +1,7 @@
 # Build examples with HEXL
 set -x
 COMPILER_FLAGS="-DCMAKE_BUILD_TYPE=Debug
+                -DMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG=build/bin/Debug
                 -DCMAKE_CXX_COMPILER=g++
                 -DCMAKE_C_COMPILER=gcc
                 -DSEAL_BUILD_TESTS=ON
