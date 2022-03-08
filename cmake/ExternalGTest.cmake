8cb10d527987be874e029359241eb0c91451acfd
Wei Dai <wei.dai@microsoft.com>
2022-03-08 15:56:05 -0800

    Updated GTest to 1.11.0.

--- cmake/ExternalGTest.cmake
+++ cmake/ExternalGTest.cmake
@@ -4,7 +4,7 @@
 FetchContent_Declare(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG        703bd9caab50b139428cea1aaff9974ebee5742e # 1.10.0
+    GIT_TAG        e2239ee6043f73722e7aa812a459f54a28552929 # 1.11.0
 )
 FetchContent_GetProperties(googletest)
 
