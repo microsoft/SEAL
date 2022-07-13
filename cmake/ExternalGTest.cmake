03a99304f382dbcbda2b9a9661961d62d0d02384
rickwebiii <rick.weber.iii@gmail.com>
2022-07-13 14:44:55 -0700

    Don't fetch deps during build; use submodules

--- cmake/ExternalGTest.cmake
+++ cmake/ExternalGTest.cmake
@@ -1,26 +1,15 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
 
-FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG        e2239ee6043f73722e7aa812a459f54a28552929 # 1.11.0
-)
-FetchContent_GetProperties(googletest)
+set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+mark_as_advanced(BUILD_GMOCK)
+mark_as_advanced(INSTALL_GTEST)
+mark_as_advanced(FETCHCONTENT_SOURCE_DIR_GOOGLETEST)
+mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_GOOGLETEST)
 
-if(NOT googletest_POPULATED)
-    FetchContent_Populate(googletest)
-
-    set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
-    set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-    mark_as_advanced(BUILD_GMOCK)
-    mark_as_advanced(INSTALL_GTEST)
-    mark_as_advanced(FETCHCONTENT_SOURCE_DIR_GOOGLETEST)
-    mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_GOOGLETEST)
-
-    add_subdirectory(
-        ${googletest_SOURCE_DIR}
-        ${THIRDPARTY_BINARY_DIR}/googletest-src
-        EXCLUDE_FROM_ALL)
-endif()
+add_subdirectory(
+    ${CMAKE_SOURCE_DIR}/thirdparty/googletest
+		${CMAKE_BINARY_DIR}/thirdparty/googletest
+    EXCLUDE_FROM_ALL)
--- cmake/ExternalMSGSL.cmake
+++ cmake/ExternalMSGSL.cmake
@@ -1,25 +1,17 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
 
-FetchContent_Declare(
-    msgsl
-    GIT_REPOSITORY https://github.com/microsoft/GSL.git
-    GIT_TAG        0f6dbc9e2915ef5c16830f3fa3565738de2a9230 # 3.1.0
-)
-FetchContent_GetProperties(msgsl)
+set(GSL_CXX_STANDARD "14" CACHE STRING "" FORCE)
+set(GSL_TEST OFF CACHE BOOL "" FORCE)
+mark_as_advanced(GSL_CXX_STANDARD )
+mark_as_advanced(GSL_TEST)
+mark_as_advanced(FETCHCONTENT_SOURCE_DIR_MSGSL)
+mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_MSGSL)
 
-if(NOT msgsl_POPULATED)
-    FetchContent_Populate(msgsl)
+add_subdirectory(
+    ${CMAKE_SOURCE_DIR}/thirdparty/msgsl
+    ${CMAKE_BINARY_DIR}/thirdparty/msgsl
+    EXCLUDE_FROM_ALL)
 
-    set(GSL_CXX_STANDARD "14" CACHE STRING "" FORCE)
-    set(GSL_TEST OFF CACHE BOOL "" FORCE)
-    mark_as_advanced(GSL_CXX_STANDARD )
-    mark_as_advanced(GSL_TEST)
-    mark_as_advanced(FETCHCONTENT_SOURCE_DIR_MSGSL)
-    mark_as_advanced(FETCHCONTENT_UPDATES_DISCONNECTED_MSGSL)
+include_directories(${CMAKE_SOURCE_DIR}/thirdparty/msgsl/include)
 
-    add_subdirectory(
-        ${msgsl_SOURCE_DIR}
-        ${msgsl_SOURCE_DIR}/../msgsl-build
-        EXCLUDE_FROM_ALL)
-endif()
